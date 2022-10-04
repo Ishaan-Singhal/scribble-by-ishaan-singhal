1091abf61629f1ab5c4c22bc6c28ee26476a23aa
Ishaan Singhal <104645943+Ishaan-Singhal@users.noreply.github.com>
2022-10-04 18:05:02 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
+
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import { setAuthHeaders } from "apis/axios";
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
     setAuthHeaders(setLoading);
-    }, []);
+  }, []);
 
-    if (loading) {
+  if (loading) {
     return <h1>Loading...</h1>;
-    }
+  }
+
   return (
     <Router>
       <Switch>
