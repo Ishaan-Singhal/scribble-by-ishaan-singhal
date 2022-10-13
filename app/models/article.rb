9d5b2fa94171bb099ac3910b9d5c134ee2320f56
Ishaan Singhal <104645943+Ishaan-Singhal@users.noreply.github.com>
2022-10-13 17:13:03 +0530

    Created Article table and added slugq

---
+++ app/models/article.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Article < ApplicationRecord
+  validates :title, :status, presence: true
+  validates :slug, uniqueness: true
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      title_slug = title.parameterize
+      regex_pattern = "slug ~* ?"
+      latest_task_slug = Article.where(
+        regex_pattern,
+        "#{title_slug}$|#{title_slug}-[0-9]+$"
+      ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
+      slug_count = 0
+      if latest_task_slug.present?
+        slug_count = latest_task_slug.split("-").last.to_i
+        only_one_slug_exists = slug_count == 0
+        slug_count = 1 if only_one_slug_exists
+      end
+      slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+      self.slug = slug_candidate
+    end
+
+    def slug_not_changed
+      if slug_changed? && self.persisted?
+        errors.add(:slug, "is immutable")
+      end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_053413) do
+ActiveRecord::Schema.define(version: 2022_10_13_085650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "articles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title", null: false
+    t.string "slug", null: false
+    t.string "status", default: "draft", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["slug"], name: "index_articles_on_slug", unique: true
+  end
 
 end
