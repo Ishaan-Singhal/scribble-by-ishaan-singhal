e17dff3d0a6a124d300f304df8d9034b0244f457
Ishaan Singhal <104645943+Ishaan-Singhal@users.noreply.github.com>
2022-10-18 13:31:09 +0530

    Made changes to Eui

--- app/javascript/src/components/EUI/Menu.jsx
+++ app/javascript/src/components/EUI/Menu.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 
-import { Typography, Accordion } from "neetoui";
+import { Accordion } from "neetoui";
 import { MenuBar } from "neetoui/layouts";
 
-import { CATEGORY_VALUES } from "./constants";
-
-const Menu = () => {
+const Menu = ({ categories }) => {
   const SHOW_MENU = true;
 
   return (
     <MenuBar showMenu={SHOW_MENU}>
       <Accordion defaultActiveKey={0}>
-        {CATEGORY_VALUES.map(category => (
+        {categories.map(category => (
           <Accordion.Item key={category.id} title={category.title}>
-            {category.articles.map((article, idx) => (
-              <Typography className="m-2" key={idx} style="body2">
-                {article}
-              </Typography>
+            {category.articles.map(article => (
+              <div
+                className="m-2 cursor-pointer"
+                key={article.id}
+                onClick={() => {}}
+              >
+                {article.title}
+              </div>
             ))}
           </Accordion.Item>
         ))}
--- app/javascript/src/components/EUI/index.jsx
+++ app/javascript/src/components/EUI/index.jsx
@@ -1,32 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { Typography, Tag, Label } from "neetoui";
+import { Typography, Tag, Label, PageLoader } from "neetoui";
 import { Container } from "neetoui/layouts";
+import { useParams } from "react-router-dom";
 
+import articlesApi from "apis/articles";
+import categoriesApi from "apis/categories";
 import { monthDateFormatter } from "utils/date";
 
-import { ARTICLE_DUMMY } from "./constants";
 import Menu from "./Menu";
 import Navbar from "./Navbar";
 
 const Eui = () => {
-  const { title, body, createdAt, category } = ARTICLE_DUMMY;
+  let { slug } = useParams();
+
+  const [categoryList, setCategoryList] = useState([]);
+  const [selectedArticle, setSelectedArticle] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchCategories = async () => {
+    try {
+      const {
+        data: { categories },
+      } = await categoriesApi.list();
+      slug = slug || categories[0].articles[0].slug;
+      logger.info(slug);
+      setCategoryList(categories);
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
+  const fetchArticle = async () => {
+    try {
+      const response = await articlesApi.show(slug);
+      setSelectedArticle(response.data.article);
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
+  const loadData = async () => {
+    try {
+      await Promise.all([fetchCategories(), fetchArticle()]);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-screen">
+        <PageLoader />
+      </div>
+    );
+  }
 
   return (
     <>
       <Navbar />
       <div className="flex">
-        <Menu />
+        <Menu
+          categories={categoryList}
+          selectedArticle={selectedArticle}
+          setSelectedArticle={setSelectedArticle}
+        />
         <Container>
           <Typography className="my-2" style="h1">
-            {title}
+            {selectedArticle.title}
           </Typography>
           <div className="flex">
-            <Tag className="mr-4" label={category} style="info" />
-            <Label>{monthDateFormatter(createdAt)}</Label>
+            <Tag
+              className="mr-4"
+              label={selectedArticle.category.title}
+              style="info"
+            />
+            <Label>{monthDateFormatter(selectedArticle.date)}</Label>
           </div>
           <Typography className="my-4" style="body2">
-            {body}
+            {selectedArticle.content}
           </Typography>
         </Container>
       </div>
